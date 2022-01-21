21f40050282e6e7ac7c61e2303e50243fc35a681
PhenoZiggy <86625568+PhenoZiggy@users.noreply.github.com>
2022-01-21 21:48:10 +0530

    edits

--- Components/Buttons/PortfolioBtn.js
+++ Components/Buttons/PortfolioBtn.js
@@ -4,7 +4,7 @@ const PortfolioBtn = () => {
     return (
         <a
             // onClick={}
-            href="#"
+            href="https://github.com/PhenoZiggy"
             className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200"
         >
             Portfolio
--- Components/Home/Welcome.js
+++ Components/Home/Welcome.js
@@ -8,7 +8,7 @@ const Welcome = () => {
             <div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
                 <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                     <span className="block">Ready to dive in?</span>
-                    <span className="block">This is my React Assignment for Company _______.</span>
+                    <span className="block">This is my React Assignment for Company SURGE GLOBAL.</span>
                 </h2>
                 <div className="mt-8 flex justify-center">
                     <div className="inline-flex rounded-md shadow">
