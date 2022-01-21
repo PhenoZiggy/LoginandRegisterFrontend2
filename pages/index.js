9eacb6a2cb2532a366dbcf64d8cd573971776a24
Sameera <Sameeranicho@gmail.com>
2022-01-21 21:45:06 +0530

    init-2

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,9 @@
-import Link from 'next/link'
+import Welcome from "../Components/Home/Welcome";
 
 export default function Home() {
-  return (
-    <div>
-      <div className="bg-white">
-        <div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            <span className="block">Ready to dive in?</span>
-            <span className="block">This is my React Assignment for Company _______.</span>
-          </h2>
-          <div className="mt-8 flex justify-center">
-            <div className="inline-flex rounded-md shadow">
-              <a
-                  href="#"
-                  className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
-              >
-                Login Portal
-              </a>
-            </div>
-            <div className="ml-3 inline-flex">
-              <a
-                  // onClick={}
-                  href="#"
-                  className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200"
-              >
-                Portfolio
-              </a>
-            </div>
-          </div>
+    return (
+        <div>
+            <Welcome/>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
